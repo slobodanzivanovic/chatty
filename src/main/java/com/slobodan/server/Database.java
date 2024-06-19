e90d64336069f2b162c828c08c9df4c5bf7432f7
Slobodan Zivanovic <172178046+slobodanzivanovic@users.noreply.github.com>
2024-06-19 21:48:05 +0200

    Added bcrypt for password hashing

--- src/main/java/com/slobodan/server/Database.java
+++ src/main/java/com/slobodan/server/Database.java
@@ -1,5 +1,8 @@
 package com.slobodan.server;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,11 +11,14 @@ import java.sql.SQLException;
 
 public class Database {
     private Connection connection;
+    @SuppressWarnings("unused")
+    private SecureRandom secureRandom;
 
     public Database(String url, String user, String password) {
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(url, user, password);
+            secureRandom = new SecureRandom();
         } catch (ClassNotFoundException | SQLException exception) {
             exception.printStackTrace();
         }
@@ -20,10 +26,11 @@ public class Database {
 
     public boolean registerUser(String username, String password) {
         try {
+            String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
             PreparedStatement statement = connection
                     .prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
             statement.setString(1, username);
-            statement.setString(2, password);
+            statement.setString(2, hashedPassword);
 
             int rowsInserted = statement.executeUpdate();
 
@@ -37,11 +44,15 @@ public class Database {
     public boolean loginUser(String username, String password) {
         try {
             PreparedStatement statement = connection
-                    .prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+                    .prepareStatement("SELECT password FROM users WHERE username = ?");
             statement.setString(1, username);
-            statement.setString(2, password);
             ResultSet resultSet = statement.executeQuery();
-            return resultSet.next();
+            if (resultSet.next()) {
+                String hashedPassword = resultSet.getString("password");
+                return BCrypt.checkpw(password, hashedPassword);
+            } else {
+                return false;
+            }
         } catch (SQLException exception) {
             exception.printStackTrace();
             return false;
